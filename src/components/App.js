1a77dd1cbd61cc7796b97bf7a4249d330b7931c0
man0ffire <iliqqnev88@gmail.com>
2017-05-22 18:45:53 +0300

    the first axaj call is now working

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,17 @@ class App extends Component {
     super(props);
 
     this.state = {
-      proba: "proba",
-      summoner: []
-    };
+      proba: 'proba',
+      summonerName: '',
+      icon: '',
+      level: ''
+    }
+    this.searchNameLevelIcon('xahko');
 
-    this.searchUsername('xahko');
+
   }
 
-  searchUsername(username){
+  searchNameLevelIcon(username){
     $.ajax({
       url: 'https://euw.api.riotgames.com/api/lol/EUW/v1.4/summoner/by-name/'+username+'?api_key='+API_KEY,
       //headers: { 'Access-Control-Allow-Origin': '*' },
@@ -24,8 +27,13 @@ class App extends Component {
       dataType: 'json',
       cache: false,
       success: function(data){
-        this.setState({summoner: data}, function(){
-          console.log(this.state);
+        let info = data[username];
+        let name = info['name'];
+        let icon = info['profileIconId'];
+        let level = info['summonerLevel'];
+        //let thename = data[username[name]];
+        this.setState({summonerName: name, icon: icon, level:level}, function(){
+          console.log(info);
         })
       }.bind(this),
       error: function(xhr, status, err){
@@ -34,10 +42,15 @@ class App extends Component {
     });
   }
 
+
   render() {
     return (
       <div className="container">
         <h2>{this.state.proba}</h2>
+        <h2>{this.state.proba2}</h2>
+        <img alt="summoner icon" src={ 'http://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/' + this.state.icon +'.png' } />
+        <h2>{this.state.summonerName}</h2>
+        <h2>{this.state.level}</h2>
       </div>
     );
   }
